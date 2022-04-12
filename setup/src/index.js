3e653987fce2ce37b4832c29db13b505a7f0a611
Muhammad Asif <asif88383@gmail.com>
2022-04-12 16:32:35 +0500

    first commit

--- setup/src/index.js
+++ setup/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import './index.css'
 import App from './App'
 import { AppProvider } from './context'
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <AppProvider>
       <App />
     </AppProvider>
   </React.StrictMode>,
-  document.getElementById('root')
 )
