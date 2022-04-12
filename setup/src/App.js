ed995da9e16151040bf0e17334a25bf808c141be
Muhammad Asif <asif88383@gmail.com>
2022-04-12 22:45:52 +0500

    final commit

--- setup/src/App.js
+++ setup/src/App.js
@@ -36,30 +36,32 @@ function App() {
 
   return (
     <main>
-      <Modal />
-      <section className='quiz'>
-        <p className='correct-answers'>
-          correct answers : {correct}/{index}
-        </p>
-        <article className='container'>
-          <h2 dangerouslySetInnerHTML={{ __html: question }} />
-          <div className='btn-container'>
-            {answers.map((answer, index) => {
-              return (
-                <button
-                  key={index}
-                  className='answer-btn'
-                  onClick={() => checkAnswer(correct_answer === answer)}
-                  dangerouslySetInnerHTML={{ __html: answer }}
-                />
-              )
-            })}
-          </div>
-        </article>
-        <button className='next-question' onClick={nextQuestion}>
-          next question
-        </button>
-      </section>
+      <div className='card'>
+        <Modal />
+        <section className='quiz'>
+          <p className='correct-answers'>
+            correct answers : {correct}/{index}
+          </p>
+          <article className='container'>
+            <h2 dangerouslySetInnerHTML={{ __html: question }} />
+            <div className='btn-container'>
+              {answers.map((answer, index) => {
+                return (
+                  <button
+                    key={index}
+                    className='answer-btn'
+                    onClick={() => checkAnswer(correct_answer === answer)}
+                    dangerouslySetInnerHTML={{ __html: answer }}
+                  />
+                )
+              })}
+            </div>
+          </article>
+          <button className='next-question' onClick={nextQuestion}>
+            next question
+          </button>
+        </section>
+      </div>
     </main>
   )
 }
